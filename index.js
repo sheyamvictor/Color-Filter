fd21c8df8f1af9842e0b132c39a2ad9177ba08bf
sheyamvictor <sheyamjoseph@gmail.com>
2024-10-26 23:40:52 +0530

    updated

---
+++ index.js
@@ -0,0 +1,29 @@
+let btn = document.getElementById("flicker")
+let code = document.getElementById("codeColor")
+let change = document.getElementById("flip")
+
+// Color Code
+let hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+//Event Listener
+  btn.addEventListener('click',function(){
+   let hexColor = "#"
+
+for(let i=0;i<6;i++){
+    hexColor += randomColor()
+}
+
+change.style.backgroundColor = hexColor
+
+
+code.innerHTML = hexColor
+
+  })
+
+
+
+// set random color function
+function randomColor(){
+    let randomIndex = Math.floor(Math.random ()*16)
+    return hex[randomIndex]
+    
+}
