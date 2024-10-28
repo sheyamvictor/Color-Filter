1a7c91f12459b1cc40ba84f76f74900426cd901e
sheyamvictor <sheyamjoseph@gmail.com>
2024-10-28 12:00:40 +0530

    modified

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ let hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
   btn.addEventListener('click',function(){
    let hexColor = "#"
 
-for(let i=0;i<6;i++){
+for(let i=1;i<=6;i++){
     hexColor += randomColor()
 }
 
 change.style.backgroundColor = hexColor
 
-
+code.style.color = hexColor
 code.innerHTML = hexColor
 
   })
